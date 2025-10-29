3c5769f38415b527ead4bb2038e45533a490ca56
Alexey Novikov <me@x3lfy.space>
2025-10-29 22:52:21 +0300

    relaxed requirements: migration

---
+++ src/GZCTF/Migrations/20251029194700_RelaxRequirements.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GZCTF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelaxRequirements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Logs",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Logs",
+                type: "character varying(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+    }
+}
